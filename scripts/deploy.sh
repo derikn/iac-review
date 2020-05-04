9562a2c92d376c46b0a251c613de1d10384d9534
dng4 <dng4@lenovo.com>
2020-05-04 17:09:47 +0800

    fix gem installs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,8 @@ git clone https://github.com/Artemmkin/raddit.git
 
 echo " ----- install app dependencies ----- "
 cd ./raddit
+sudo gem install bcrypt -v '3.1.13' --source 'https://rubygems.org/'
+sudo gem install nio4r -v '2.5.2' --source 'https://rubygems.org/'
 sudo bundle install 
 
 echo " ----- start app ----- "
