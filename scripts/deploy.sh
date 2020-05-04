9dc02a97b7ffddcb0c98fbfb3cf0462c7acfeaee
dng4 <dng4@lenovo.com>
2020-05-04 17:37:20 +0800

    save own systemd service

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,7 @@ sudo gem install bcrypt -v '3.1.13' --source 'https://rubygems.org/'
 sudo gem install nio4r -v '2.5.2' --source 'https://rubygems.org/'
 sudo gem install bson -v '4.8.2' --source 'https://rubygems.org/'
 sudo gem install puma -v '4.3.3' --source 'https://rubygems.org/'
+sudo gem install bson_ext -v '1.5.1' --source 'https://rubygems.org/'
 sudo bundle install 
 
 echo " ----- start app ----- "
