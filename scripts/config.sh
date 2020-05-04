9babd2696dd7e91c43a0e0263b5ce585f81edbfd
dng4 <dng4@lenovo.com>
2020-05-04 16:58:04 +0800

    fix mongodb scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,12 +4,12 @@ set -e
 echo " ----- install ruby and bundler ----- "
 sudo apt-get update
 sudo apt-get install -y ruby-full build-essential
-gem install --no-rdoc --no-ri bundler
+sudo gem install --no-rdoc --no-ri bundler
 
 echo " ----- install mongodb ----- "
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
